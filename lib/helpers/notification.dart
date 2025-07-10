840f5c2bf6cc5c66a2e53cdb2610af6e2bfc96ad
Denny Octavian <dennyoctavian.nutech@gmail.com>
2025-07-10 15:36:46 +0700

    add random api

--- lib/helpers/notification.dart
+++ lib/helpers/notification.dart
@@ -1,12 +1,26 @@
+import 'dart:math';
+
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:restaurant_app/data/services/services.dart';
 import 'package:restaurant_app/helpers/local_notification_service.dart';
 import 'package:timezone/timezone.dart' as tz;
 
 class NotificationHelper {
   static Future<void> scheduleDailyReminder() async {
+    ApiRestaurant apiRestaurant = ApiRestaurant();
+    final response = await apiRestaurant.fetchListRestaurant();
+    final random = Random();
+
+    final indexRandom = random.nextInt((response.restaurants ?? []).length);
+
+    String nameRestaurant = "KFC";
+    if (indexRandom > 0) {
+      nameRestaurant = response.restaurants?[indexRandom].name ?? "KFC";
+    }
+
     await flutterLocalNotificationsPlugin.zonedSchedule(
       0,
-      'Waktunya Makan Siang!',
+      'Waktunya Makan Siang di $nameRestaurant!',
       'Yuk makan yang sehat hari ini 🍱',
       _nextInstanceOf11AM(),
       const NotificationDetails(
